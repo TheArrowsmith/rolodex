1c08b254b3f2f3ab85c40960e6faf9b4077a6b80
George <george@arrowsmith.codes>
2025-05-19 15:23:39 +0100

    starter app

---
+++ test/rolodex_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule RolodexWeb.ErrorJSONTest do
+  use RolodexWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert RolodexWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert RolodexWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
